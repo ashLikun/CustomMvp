228e84d8215aa36528ae95bad811caac1994f530
likun <496546144@qq.com>
2018-08-20 22:46:56 +0800

    优化

--- app/src/main/java/com/ashlikun/core/simple/MainActivity.java
+++ app/src/main/java/com/ashlikun/core/simple/MainActivity.java
@@ -7,7 +7,6 @@ import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.ashlikun.core.activity.BaseMvpActivity;
-import com.ashlikun.core.simple.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseMvpActivity<MainPresenter>
         implements IMainView {
